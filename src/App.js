2a30e74777f34fe1622e53597461d774f6d45016
Tanishq Jain <Tanishq.Jain@Sobeys.com>
2021-08-12 18:23:40 +0530

    Starting with states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
-  const expenses = [
+  const user_expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -24,29 +24,7 @@ function App() {
   ];
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </div>
+    <Expenses expenses={user_expenses}></Expenses>
   );
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  
+
+  //This function here is used initialize state change for a prop element
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler(){
+    setTitle('Updated!')
+    console.log(title);
+  }
 
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
